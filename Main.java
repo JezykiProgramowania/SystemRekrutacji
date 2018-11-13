951134bfa40b4864f6f5114d43c75ac52062b5b7
Krystian_Wojakiewicz <37145346+KrystianWojakiewicz@users.noreply.github.com>
2018-11-14 00:47:10 +0100

    this is a rescue file. Trying a different approach

--- Main.java
+++ Main.java
@@ -1,9 +1,6 @@
 
 import java.io.*;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class Main {
@@ -33,22 +30,160 @@ public class Main {
             }
 
 
-            if(licznik == 0) ((LinkedList<Uczen>) listaUczniow).getLast().SetImie(scanner.next()); // sets name
+            if (licznik == 0) ((LinkedList<Uczen>) listaUczniow).getLast().SetImie(scanner.next()); // sets name
 
-            if(licznik == 1)((LinkedList<Uczen>) listaUczniow).getLast().SetPunkty(scanner.nextInt()); // sets points
+            if (licznik == 1) ((LinkedList<Uczen>) listaUczniow).getLast().SetPunkty(scanner.nextInt()); // sets points
 
-            if(licznik > 1)((LinkedList<Uczen>) listaUczniow).getLast().SetPrefSzkola(scanner.nextInt(), scanner.next()); // stores school preference as a map
+            if (licznik > 1)
+                ((LinkedList<Uczen>) listaUczniow).getLast().SetSzkolaPref(scanner.next(), scanner.nextInt()); // stores school preference as a map
 
             licznik++;
         }
 
-        /////////////////////
-        ////////////////
-        //TESTY
-        ////////////////////
-       System.out.println("to jest uczen nr 0: " + listaUczniow.get(1).GetImie());
-        System.out.println("to jest uczen nr 0: " + listaUczniow.get(0).GetPrefSzkola().get(1));
-        //System.out.println("to jest uczen nr 1: " + listaUczniow.get(1).GetPreferencje());
 
-    }
-}
+        int counter = 0;
+        List<Integer> min = new ArrayList<>();
+        for(Uczen uczen : listaUczniow) {
+
+            min.add(Collections.min(uczen.GetSzkolaPref().values()));
+
+        }
+        while(counter < 1) {
+            for (int i = 0; i < listaUczniow.size(); i++) {
+                for (int j = 0; j < listaUczniow.size(); j++) {
+                    // check for highest scores first, then preference
+
+
+                    if (listaUczniow.get(i).GetSzkolaPref().get("szkola1").equals(listaUczniow.get(j).GetSzkolaPref().get("szkola1"))) {
+                        if (!listaUczniow.get(j).isSatisfied() || !listaUczniow.get(i).isSatisfied()) {
+
+                            if(listaUczniow.get(i).isSatisfied()) listaUczniow.get(j).SetWynikRekrutacji("szkola1");
+
+                            if(!listaUczniow.get(i).isSatisfied() && listaUczniow.get(j).GetPunkty() > listaUczniow.get(i).GetPunkty()) listaUczniow.get(j).SetWynikRekrutacji("szkola1");
+
+                            if(listaUczniow.get(j).GetPunkty() < listaUczniow.get(i).GetPunkty() && listaUczniow.get(i).isSatisfied() && listaUczniow.get(i).GetWynikRekrutacji().equals("szkola1")) {
+                                int temp = min.get(j);
+                                min.set(j, temp++);
+                            }
+
+                            if(listaUczniow.get(j).GetPunkty() > listaUczniow.get(i).GetPunkty() && listaUczniow.get(i).isSatisfied())  {
+
+                                listaUczniow.get(i).setSatisfied(false);
+                                int temp = min.get(j);
+                                
+                                min.set(i, temp++);
+
+                                listaUczniow.get(i).SetWynikRekrutacji("");
+                                listaUczniow.get(j).SetWynikRekrutacji("szkola1");
+                            }
+
+                                if (listaUczniow.get(j).GetSzkolaPref().get("szkola1") == min.get(j)) {
+
+                                    listaUczniow.get(j).setSatisfied(true);
+
+
+
+
+                            }
+                        }
+                    }
+
+
+                    if (listaUczniow.get(i).GetSzkolaPref().get("szkola2").equals(listaUczniow.get(j).GetSzkolaPref().get("szkola2"))) {
+                        if (!listaUczniow.get(j).isSatisfied() || !listaUczniow.get(i).isSatisfied()) {
+
+                            if(listaUczniow.get(i).isSatisfied()) listaUczniow.get(j).SetWynikRekrutacji("szkola2");
+                            if(!listaUczniow.get(i).isSatisfied() && listaUczniow.get(j).GetPunkty() > listaUczniow.get(i).GetPunkty()) listaUczniow.get(j).SetWynikRekrutacji("szkola2");
+                            if(listaUczniow.get(j).GetPunkty() < listaUczniow.get(i).GetPunkty() && !listaUczniow.get(i).isSatisfied()) {
+                                int temp = min.get(j);
+                                min.set(j, temp++);
+                                listaUczniow.get(i).SetWynikRekrutacji("szkola2");
+                            }
+                            if(listaUczniow.get(j).GetPunkty() > listaUczniow.get(i).GetPunkty() && listaUczniow.get(i).isSatisfied())  {
+
+                                listaUczniow.get(i).setSatisfied(false);
+                                int temp = min.get(i);
+                                min.set(i, temp++);
+                                listaUczniow.get(i).SetWynikRekrutacji("");
+                                listaUczniow.get(j).SetWynikRekrutacji("szkola2");
+                            }
+                                if (listaUczniow.get(j).GetSzkolaPref().get("szkola2") == min.get(j)) {
+                                    listaUczniow.get(j).setSatisfied(true);
+
+
+
+
+                            }
+                        }
+                    }
+
+                    if (listaUczniow.get(i).GetSzkolaPref().get("szkola3").equals(listaUczniow.get(j).GetSzkolaPref().get("szkola3"))) {
+                        if (!listaUczniow.get(j).isSatisfied() || !listaUczniow.get(i).isSatisfied()) {
+
+                            if(listaUczniow.get(i).isSatisfied()) listaUczniow.get(j).SetWynikRekrutacji("szkola3");
+
+                            if(!listaUczniow.get(i).isSatisfied() && listaUczniow.get(j).GetPunkty() > listaUczniow.get(i).GetPunkty()) listaUczniow.get(j).SetWynikRekrutacji("szkola3");
+
+                            if(listaUczniow.get(j).GetPunkty() < listaUczniow.get(i).GetPunkty() && !listaUczniow.get(i).isSatisfied()) {
+                                int temp = min.get(i);
+                                min.set(j, temp++);
+                                listaUczniow.get(i).SetWynikRekrutacji("szkola3");
+                            }
+                            if(listaUczniow.get(j).GetPunkty() > listaUczniow.get(i).GetPunkty() && listaUczniow.get(i).isSatisfied())  {
+
+                                listaUczniow.get(i).setSatisfied(false);
+                                int temp = min.get(i);
+                                min.set(i, temp++);
+                                listaUczniow.get(i).SetWynikRekrutacji("");
+                                listaUczniow.get(j).SetWynikRekrutacji("szkola3");
+                            }
+
+                                if (listaUczniow.get(j).GetSzkolaPref().get("szkola3") == min.get(j)) {
+                                     listaUczniow.get(j).setSatisfied(true);
+
+                            }
+                        }
+                    }
+
+/*
+                    // now check for preference
+                    if (listaUczniow.get(j).GetSzkolaPref().get("szkola1") < listaUczniow.get(i).GetSzkolaPref().get("szkola1") && !listaUczniow.get(j).isSatisfied()) {
+                        listaUczniow.get(j).SetWynikRekrutacji("szkola1");
+                        if (listaUczniow.get(j).GetSzkolaPref().get("szkola1").equals(Collections.min(listaUczniow.get(j).GetSzkolaPref().values())))
+                            listaUczniow.get(j).setSatisfied(true);
+                    }
+                    if (listaUczniow.get(j).GetSzkolaPref().get("szkola2") < listaUczniow.get(i).GetSzkolaPref().get("szkola2") && !listaUczniow.get(j).isSatisfied()) {
+                        listaUczniow.get(j).SetWynikRekrutacji("szkola2");
+                        if (listaUczniow.get(j).GetSzkolaPref().get("szkola2").equals(Collections.min(listaUczniow.get(j).GetSzkolaPref().values())))
+                            listaUczniow.get(j).setSatisfied(true);
+
+                    }
+                    if (listaUczniow.get(j).GetSzkolaPref().get("szkola3") < listaUczniow.get(i).GetSzkolaPref().get("szkola3") && !listaUczniow.get(j).isSatisfied()) {
+                        listaUczniow.get(j).SetWynikRekrutacji("szkola3");
+                        if (listaUczniow.get(j).GetSzkolaPref().get("szkola3").equals(Collections.min(listaUczniow.get(j).GetSzkolaPref().values())))
+                            listaUczniow.get(j).setSatisfied(true);
+
+                    }*/
+                }
+
+            }
+        counter++;
+        }
+                /////////////////////
+                ////////////////
+                //TESTY
+                ////////////////////
+        for(Uczen uczen : listaUczniow) {
+            uczen.GetSzkolaPref().remove("szkola1");
+        }
+
+                System.out.println("to jest uczen nr 1: " + listaUczniow.get(1).GetImie());
+                System.out.println("to jest uczen nr 0: " + listaUczniow.get(0).GetSzkolaPref().get("szkola1"));
+                System.out.println("wynik rekrutacji uczen nr 0: " + listaUczniow.get(0).GetWynikRekrutacji());
+                System.out.println("wynik rekrutacji uczen nr 1: " + listaUczniow.get(1).GetWynikRekrutacji());
+                System.out.println("wynik rekrutacji uczen nr 2: " + listaUczniow.get(2).GetWynikRekrutacji());
+                System.out.println("min value: " + min);
+
+
+            }
+        }
