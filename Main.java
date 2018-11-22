4da7cc4c65ba92dfe2496ff5efc9d2ad7fddef59
Krystian_Wojakiewicz <37145346+KrystianWojakiewicz@users.noreply.github.com>
2018-11-22 01:22:15 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,188 +2,191 @@
 import java.io.*;
 import java.util.*;
 import java.util.regex.Pattern;
-
+import java.util.stream.Collectors;
 public class Main {
 
+
+
+
     public static void main(String[] args) throws IOException {
-        FileReader plik = new FileReader("dane.txt");
-        BufferedReader bufor = new BufferedReader(plik);
-        String readLine = "";
 
-
-        Scanner scanner = new Scanner(new File("dane.txt"));  //opening the file stream
+        Scanner scanner = new Scanner(new File("data.txt"));  //opening the file stream
         scanner.useDelimiter(System.lineSeparator());
-
-        int licznik = 0;
-        List<Uczen> listaUczniow = new LinkedList<>(); // list of students
         Pattern pattern = Pattern.compile(";"); //sets the pattern for student separation in the file
+
+        Scanner scanner2 = new Scanner(new File("schools.txt"));
+
+
+        List<Student> studentsList = new LinkedList<>(); // list of students
+        List<School> schoolsList = new LinkedList<>(); // list of school objects
+        List<String> schools = new LinkedList<>(); // list of schools
+
+
+
+
+        int counter = 0;
         while (scanner.hasNext()) {
 
             if (scanner.hasNext(pattern)) {
-                // if one student's info ends (which is signalled by a semicolon in dane.txt) create a new Student
+                // if one student's info ends (which is signalled by a semicolon in data.txt) create a new Student
                 // Object and add it to the Students list
-                ((LinkedList<Uczen>) listaUczniow).addLast(new Uczen());
+                ((LinkedList<Student>) studentsList).addLast(new Student());
 
                 scanner.next(); // skip the semicolon
-                licznik = 0; // reset the counter to allow for name and points to be set
+                counter = 0; // reset the counter to allow for name and points to be set
                 continue;
             }
 
 
-            if (licznik == 0) ((LinkedList<Uczen>) listaUczniow).getLast().SetImie(scanner.next()); // sets name
+            if (counter == 0) ((LinkedList<Student>) studentsList).getLast().setName(scanner.next()); // sets name
 
-            if (licznik == 1) ((LinkedList<Uczen>) listaUczniow).getLast().SetPunkty(scanner.nextInt()); // sets points
+            if (counter == 1) ((LinkedList<Student>) studentsList).getLast().setPoints(scanner.nextInt()); // sets points
 
-            if (licznik > 1)
-                ((LinkedList<Uczen>) listaUczniow).getLast().SetSzkolaPref(scanner.next(), scanner.nextInt()); // stores school preference as a map
+            if (counter > 1)
+                ((LinkedList<Student>) studentsList).getLast().setPreferredSchool(scanner.nextInt(), scanner.next()); // stores school preference as a map
 
-            licznik++;
+            counter++;
         }
 
 
-        int counter = 0;
-        List<Integer> min = new ArrayList<>();
-        for(Uczen uczen : listaUczniow) {
-
-            min.add(Collections.min(uczen.GetSzkolaPref().values()));
-
+        while (scanner2.hasNext()) {
+            ((LinkedList<String>) schools).add(scanner2.next());
         }
-        while(counter < 1) {
-            for (int i = 0; i < listaUczniow.size(); i++) {
-                for (int j = 0; j < listaUczniow.size(); j++) {
-                    // check for highest scores first, then preference
-
-
-                    if (listaUczniow.get(i).GetSzkolaPref().get("szkola1").equals(listaUczniow.get(j).GetSzkolaPref().get("szkola1"))) {
-                        if (!listaUczniow.get(j).isSatisfied() || !listaUczniow.get(i).isSatisfied()) {
-
-                            if(listaUczniow.get(i).isSatisfied()) listaUczniow.get(j).SetWynikRekrutacji("szkola1");
-
-                            if(!listaUczniow.get(i).isSatisfied() && listaUczniow.get(j).GetPunkty() > listaUczniow.get(i).GetPunkty()) listaUczniow.get(j).SetWynikRekrutacji("szkola1");
-
-                            if(listaUczniow.get(j).GetPunkty() < listaUczniow.get(i).GetPunkty() && listaUczniow.get(i).isSatisfied() && listaUczniow.get(i).GetWynikRekrutacji().equals("szkola1")) {
-                                int temp = min.get(j);
-                                min.set(j, temp++);
-                            }
-
-                            if(listaUczniow.get(j).GetPunkty() > listaUczniow.get(i).GetPunkty() && listaUczniow.get(i).isSatisfied())  {
-
-                                listaUczniow.get(i).setSatisfied(false);
-                                int temp = min.get(j);
-                                
-                                min.set(i, temp++);
-
-                                listaUczniow.get(i).SetWynikRekrutacji("");
-                                listaUczniow.get(j).SetWynikRekrutacji("szkola1");
-                            }
-
-                                if (listaUczniow.get(j).GetSzkolaPref().get("szkola1") == min.get(j)) {
-
-                                    listaUczniow.get(j).setSatisfied(true);
 
 
 
-
-                            }
-                        }
-                    }
-
-
-                    if (listaUczniow.get(i).GetSzkolaPref().get("szkola2").equals(listaUczniow.get(j).GetSzkolaPref().get("szkola2"))) {
-                        if (!listaUczniow.get(j).isSatisfied() || !listaUczniow.get(i).isSatisfied()) {
-
-                            if(listaUczniow.get(i).isSatisfied()) listaUczniow.get(j).SetWynikRekrutacji("szkola2");
-                            if(!listaUczniow.get(i).isSatisfied() && listaUczniow.get(j).GetPunkty() > listaUczniow.get(i).GetPunkty()) listaUczniow.get(j).SetWynikRekrutacji("szkola2");
-                            if(listaUczniow.get(j).GetPunkty() < listaUczniow.get(i).GetPunkty() && !listaUczniow.get(i).isSatisfied()) {
-                                int temp = min.get(j);
-                                min.set(j, temp++);
-                                listaUczniow.get(i).SetWynikRekrutacji("szkola2");
-                            }
-                            if(listaUczniow.get(j).GetPunkty() > listaUczniow.get(i).GetPunkty() && listaUczniow.get(i).isSatisfied())  {
-
-                                listaUczniow.get(i).setSatisfied(false);
-                                int temp = min.get(i);
-                                min.set(i, temp++);
-                                listaUczniow.get(i).SetWynikRekrutacji("");
-                                listaUczniow.get(j).SetWynikRekrutacji("szkola2");
-                            }
-                                if (listaUczniow.get(j).GetSzkolaPref().get("szkola2") == min.get(j)) {
-                                    listaUczniow.get(j).setSatisfied(true);
+        for (Student s : studentsList) {
+            s.setSortedSchools(new ArrayList<String>(s.getPreferredSchool().values())); // sorting the keys so that the lowest jey is first
+        }
 
 
 
+         Map<Integer, String>pointsToNameMap = new TreeMap<>();
 
-                            }
-                        }
-                    }
+        int c = 0;
+        while(c < studentsList.size()) {
+            pointsToNameMap.put(studentsList.get(c).getPoints(), studentsList.get(c).getName());
+            c++;
+        }
 
-                    if (listaUczniow.get(i).GetSzkolaPref().get("szkola3").equals(listaUczniow.get(j).GetSzkolaPref().get("szkola3"))) {
-                        if (!listaUczniow.get(j).isSatisfied() || !listaUczniow.get(i).isSatisfied()) {
+        int a = 0;
+        while (a < schools.size()) {
+            ((LinkedList<School>) schoolsList).addLast(new School(new ArrayList<>(pointsToNameMap.values()), schools.get(a)));
+            a++;
+        }
 
-                            if(listaUczniow.get(i).isSatisfied()) listaUczniow.get(j).SetWynikRekrutacji("szkola3");
+        //////////////////////////////////////////////////////////////////////////////
+        boolean [] isSatisfied = new boolean[studentsList.size()];
+      for(int i = 0; i < isSatisfied.length;i++) {
+          isSatisfied[i] = false;
+      }
 
-                            if(!listaUczniow.get(i).isSatisfied() && listaUczniow.get(j).GetPunkty() > listaUczniow.get(i).GetPunkty()) listaUczniow.get(j).SetWynikRekrutacji("szkola3");
 
-                            if(listaUczniow.get(j).GetPunkty() < listaUczniow.get(i).GetPunkty() && !listaUczniow.get(i).isSatisfied()) {
-                                int temp = min.get(i);
-                                min.set(j, temp++);
-                                listaUczniow.get(i).SetWynikRekrutacji("szkola3");
-                            }
-                            if(listaUczniow.get(j).GetPunkty() > listaUczniow.get(i).GetPunkty() && listaUczniow.get(i).isSatisfied())  {
+        /////////////////////////////////////////////
+        // ALGORYTM START
+        //////////////////////////////////////////////
+        ComputeResults(studentsList,schoolsList,isSatisfied);
 
-                                listaUczniow.get(i).setSatisfied(false);
-                                int temp = min.get(i);
-                                min.set(i, temp++);
-                                listaUczniow.get(i).SetWynikRekrutacji("");
-                                listaUczniow.get(j).SetWynikRekrutacji("szkola3");
-                            }
+        /////////////////////////////////////////
+        //WYNIK DZIALANIA PROGRAMU
+        /////////////////////////
+        ShowResults(schoolsList);
 
-                                if (listaUczniow.get(j).GetSzkolaPref().get("szkola3") == min.get(j)) {
-                                     listaUczniow.get(j).setSatisfied(true);
 
-                            }
-                        }
-                    }
-
+         /////////////////////
+         ////////////////
+         //TESTY
+        ////////////////////
 /*
-                    // now check for preference
-                    if (listaUczniow.get(j).GetSzkolaPref().get("szkola1") < listaUczniow.get(i).GetSzkolaPref().get("szkola1") && !listaUczniow.get(j).isSatisfied()) {
-                        listaUczniow.get(j).SetWynikRekrutacji("szkola1");
-                        if (listaUczniow.get(j).GetSzkolaPref().get("szkola1").equals(Collections.min(listaUczniow.get(j).GetSzkolaPref().values())))
-                            listaUczniow.get(j).setSatisfied(true);
-                    }
-                    if (listaUczniow.get(j).GetSzkolaPref().get("szkola2") < listaUczniow.get(i).GetSzkolaPref().get("szkola2") && !listaUczniow.get(j).isSatisfied()) {
-                        listaUczniow.get(j).SetWynikRekrutacji("szkola2");
-                        if (listaUczniow.get(j).GetSzkolaPref().get("szkola2").equals(Collections.min(listaUczniow.get(j).GetSzkolaPref().values())))
-                            listaUczniow.get(j).setSatisfied(true);
+                 System.out.println("mapa przed usunieciem " + studentsList.get(0).getSortedSchools());
+                studentsList.get(0).getSortedSchools().remove(0);
+                System.out.println("mapa po usunieciu " + studentsList.get(0).getSortedSchools().get(0));
+                System.out.println("imie ucznia nr 0 " + studentsList.get(0).getName());
+                System.out.println("punkty ucznia nr 0 " + studentsList.get(0).getPoints());
+                System.out.println(" preferencje ucznia nr 0: " + studentsList.get(0).getPreferredSchool());
+                System.out.println(" posortowane preferencje ucznia nr 0: " + studentsList.get(0).getSortedSchools());
+                System.out.println(" lista mozliwych szkol:  " + schools);
+                System.out.println(" pointsToName:  " + pointsToNameMap);
+                System.out.println(" preferredStudents:  " + schoolsList.get(0).getPreferredStudents());
+                int n = IndexOfSchoolString("szkola1", schools);
+                int x = IndexOfSchool(studentsList.get(0).getSortedSchools().get(0), schoolsList);
+                System.out.println("IndexOF = " + x);
+                System.out.println("eewwrgrrgrgrw = " +schoolsList.get(1).getSchoolName());
+                System.out.println("schoolname = " + schoolsList.get(0).getSchoolName());
+*/
+    }
 
-                    }
-                    if (listaUczniow.get(j).GetSzkolaPref().get("szkola3") < listaUczniow.get(i).GetSzkolaPref().get("szkola3") && !listaUczniow.get(j).isSatisfied()) {
-                        listaUczniow.get(j).SetWynikRekrutacji("szkola3");
-                        if (listaUczniow.get(j).GetSzkolaPref().get("szkola3").equals(Collections.min(listaUczniow.get(j).GetSzkolaPref().values())))
-                            listaUczniow.get(j).setSatisfied(true);
 
-                    }*/
+
+
+    public static void ComputeResults(List<Student> studentsList, List<School> schoolsList, boolean[] isSatisfied) {
+        int endCondition = 0;
+        while (endCondition < studentsList.size()) {
+            for (int i = 0; i < studentsList.size(); i++) {
+                if (schoolsList.get(IndexOfSchool(studentsList.get(i).getSortedSchools().get(0), schoolsList)).currentStudent.equals("")) {
+                    schoolsList.get(IndexOfSchool(studentsList.get(i).getSortedSchools().get(0), schoolsList)).currentStudent = studentsList.get(i).getName();
+                    isSatisfied[IndexOfStudent(schoolsList.get(IndexOfSchool(studentsList.get(i).getSortedSchools().get(0), schoolsList)).currentStudent, studentsList)] = true;
+                } else {
+                    if (studentsList.get(IndexOfStudent(schoolsList.get(IndexOfSchool(studentsList.get(i).getSortedSchools().get(0), schoolsList)).currentStudent, studentsList)).getPoints()
+                            < studentsList.get(i).getPoints()) {
+                        isSatisfied[IndexOfStudent(schoolsList.get(IndexOfSchool(studentsList.get(i).getSortedSchools().get(0), schoolsList)).currentStudent, studentsList)] = false;
+                        studentsList.get(IndexOfStudent(schoolsList.get(IndexOfSchool(studentsList.get(i).getSortedSchools().get(0), schoolsList)).currentStudent, studentsList)).getSortedSchools().remove(0);
+                        schoolsList.get(IndexOfSchool(studentsList.get(i).getSortedSchools().get(0), schoolsList)).currentStudent = studentsList.get(i).getName();
+                        schoolsList.get(IndexOf(studentsList.get(i).getName(), schoolsList)).currentStudent = "";
+                    } else if (studentsList.get(IndexOfStudent(schoolsList.get(IndexOfSchool(studentsList.get(i).getSortedSchools().get(0), schoolsList)).currentStudent, studentsList)).getPoints()
+                            > studentsList.get(i).getPoints()) {
+
+                        studentsList.get(i).getSortedSchools().remove(0);
+                    }
                 }
-
             }
-        counter++;
+            endCondition = endCondition(studentsList, isSatisfied);
         }
-                /////////////////////
-                ////////////////
-                //TESTY
-                ////////////////////
-        for(Uczen uczen : listaUczniow) {
-            uczen.GetSzkolaPref().remove("szkola1");
+    }
+
+//HELPER FUNCTIONS
+    public static int endCondition(List<Student> list, boolean[] isSatisfied) {
+        int endCondition = 0;
+        for (int j = 0; j < list.size(); j++) {
+            if (isSatisfied[j] == true) endCondition++;
         }
+        return endCondition;
+    }
+    public static int IndexOfSchool(String s, List<School> list){
+        for(int i = 0; i < list.size();i++) {
+            if(list.get(i).getSchoolName().equals(s)) return i;
+        }
+        return -1;
+    }
 
-                System.out.println("to jest uczen nr 1: " + listaUczniow.get(1).GetImie());
-                System.out.println("to jest uczen nr 0: " + listaUczniow.get(0).GetSzkolaPref().get("szkola1"));
-                System.out.println("wynik rekrutacji uczen nr 0: " + listaUczniow.get(0).GetWynikRekrutacji());
-                System.out.println("wynik rekrutacji uczen nr 1: " + listaUczniow.get(1).GetWynikRekrutacji());
-                System.out.println("wynik rekrutacji uczen nr 2: " + listaUczniow.get(2).GetWynikRekrutacji());
-                System.out.println("min value: " + min);
+    public static int IndexOfSchoolString(String s, List<String> list){
+        for(int i = 0; i < list.size();i++) {
+            if(list.get(i).equals(s)) return i;
+        }
+        return -1;
+    }
+    public static int IndexOfStudent(String s, List<Student> list) {
+
+        for(int i = 0; i < list.size();i++) {
+            if(list.get(i).getName().equals(s)) return i;
+        }
+        return -1;
+    }
+    public static int IndexOf(String s, List<School> list) {
+
+        for(int i = 0; i < list.size(); i++) {
+            if(list.get(i).currentStudent.equals(s)) return i;
+        }
+        return -1;
+    }
 
 
-            }
+    public static void ShowResults(List<School> list) {
+        System.out.println("szkola: " + list.get(0).getSchoolName());
+        System.out.println("wynik: " + list.get(0).currentStudent + "\n");
+        System.out.println("szkola: " + list.get(1).getSchoolName());
+        System.out.println("wynik: " + list.get(1).currentStudent + "\n");
+        System.out.println("szkola: " + list.get(2).getSchoolName());
+        System.out.println("wynik: " + list.get(2).currentStudent);
+    }
         }
